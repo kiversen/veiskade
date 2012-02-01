03425df5c59f6422a3a91da57cdfa85a8ace7a55
Kent Iversen <kent.iversen@webstep.no>
2012-02-01 10:52:39 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Veiskade::Application.config.secret_token = 'bcbde92301b374524a55de22500f3bfc774bf662c0d9d262bf565eabc74e3204b55bdd754ea202329261057d47f983916ad96dd542e2e8d79913fa369200aa38'
